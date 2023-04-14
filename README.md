38cffce00d4ec9d73ce926a55048e5d20d3e9b09
MANU564 <manuelhf564@gmail.com>
2023-04-13 19:04:57 -0500

    se crea un modal del formulaio de registro con su boton, junto con el ts del mismo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Este repositorio esta creado para subir el taller 2 de curso de front end developer
+Taller 2
+Este poyecto es de tipescript
+
+Para ejecutar el proyecto, siga las siguientes instrucciones 
+
+1. Realice el git clone del proyecto
+2. Ingrese a la carpeta creada y ejecute npm install para crear la carpeta node_modules
+3. Ejecute el comando tsc para compilar los archivos tsc y crear la carpeta dist/
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hola");
+const form = document.getElementById('form') as HTMLFormElenent;
+const btnGuardar = document.getElementById('btn-guardar') as HTMLButtonElenent;
+
+btnGuardar.addEventListener('click', (e) => 
+e.preventDefault();
+console.log("Ingreso a la validación del formulario"); {
+
+
+const tipobocumento = document.getElementByTd('tipoDocumento') as HTMLInputElemen
+const numeroDocumento = document.getElementBy1d('numeroDocumento') as NTMLInputElement;
+const nombre = document.getElementBy1d('nombre') as HTMLInputElement;
+const apellido = document.getElementById('apellido') as MTMLINputElement;
+const celular = document.getElementByTd('celutar') as HTMLInputElement;
+const email = document.getElementById('enail') as HTMLInputElement;
+const Linkedin = document.getElementById('Linkedin') as HTMLInputElement;
+const github = document.getElementByTd('gíthub') as HTHLInputElement;
+
+console.log(tipoDocumento.value) ;
+
+})
